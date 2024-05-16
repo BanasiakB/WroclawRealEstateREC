68600a7855d0009fa63852c93512614694b5f028
Bartek Rozycki <rozycki1232@gmail.com>
2024-05-16 21:56:37 +0200

    Move url request and soup creation functions to separate file

--- collect_data/scrap.py
+++ collect_data/scrap.py
@@ -1,31 +1,9 @@
 from collect_data.googledrive import upload_image_to_drive
+from collect_data.scrap_utils import request_url_get_soup
 # import pandas as pd
 import time
-import random
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-
-userAgents=['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0'
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Edg/124.0.0.0',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36']
-
-
-def request_url(url: str):
-    response = requests.get(url, headers={'User-Agent': random.choice(userAgents)})
-    
-    if response.status_code != 200:
-        raise ConnectionError(f"Failed to fetch search results. Status code: {response.status_code}")
-    return response
-
-def request_url_get_soup(url: str):
-    response = request_url(url=url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    return soup
 
 
 def scrapper() -> None:
