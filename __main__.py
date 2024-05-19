c54dafbf6c5bdd2ffbcf77a14b0aefc3878794c0
Małgorzata <mal.jakubik@gmail.com>
2024-05-19 12:46:22 +0200

    revert changes on __main__

--- __main__.py
+++ __main__.py
@@ -1,7 +1,4 @@
 from collect_data.scrap import scrapper
-from collect_data.converters.convert_data import *
 
 if __name__ == "__main__":
-    # scrapper()
-    tmp = Converter(findings)
-    print(tmp.convert_all())
+    scrapper()
